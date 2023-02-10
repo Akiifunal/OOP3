bf731e983244f4d07d184439fd29e2a57ee42799
Akiifunal <akif_unal@icloud.com>
2023-02-10 17:20:37 +0300

    Add project files.

---
+++ OOP3/EsnafKredisi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    class EsnafKredisi : IKrediManager
+    {
+        public void BiSeyYap()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Hesapla()
+        {
+            Console.WriteLine("Esnaf kredisi hesaplandı");
+        }
+    }
+}
